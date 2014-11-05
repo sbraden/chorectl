e3bc0921d653dd697f9992ca7d8bf204e7084824
Sarah Braden <sarah.braden@hireiqinc.com>
2014-11-04 18:33:01 -0700

    setup sqlalchemy and flask migrate

--- chorectl/app.py
+++ chorectl/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from .frontend import frontend
+from .models.db import db
 
 
 def create_app():
     app = Flask(__name__)
+    db.init_app(app)
     app.register_blueprint(frontend)
     return app
--- manage.py
+++ manage.py
@@ -1,9 +1,14 @@
+from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
 from chorectl.app import create_app
+from chorectl.models.db import db
 
 
 app = create_app()
+migrate = Migrate(app, db)
 manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
